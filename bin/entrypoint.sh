61f1758d05ecad6bf42002a55205f387830c232b
dan <honghao.shan@duitang.com>
2016-10-19 18:13:26 +0800

    feature update docker

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -3,6 +3,9 @@
 service nginx start
 
 /tmp/qshell/qshell_linux_amd64 account $QINIU_ACCESS_KEY $QINIU_SECRET_KEY
+
+sed -i 's/QINIU_ACCESS_KEY/'"$QINIU_ACCESS_KEY"'/g' /jekyll/qiniu/qupload_*.json
+sed -i 's/QINIU_SECRET_KEY/'"$QINIU_SECRET_KEY"'/g' /jekyll/qiniu/qupload_*.json
 /tmp/qshell/qshell_linux_amd64 qupload /jekyll/qiniu/qupload_assets.json
 /tmp/qshell/qshell_linux_amd64 qupload /jekyll/qiniu/qupload_files.json
 
