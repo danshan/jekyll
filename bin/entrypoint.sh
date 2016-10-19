3f095f983cd5c75beec79159ab6758b30f743c7d
dan <honghao.shan@duitang.com>
2016-10-19 17:47:36 +0800

    feature update qiniu cmd

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -2,9 +2,9 @@
 
 service nginx start
 
-/bin/bash /tmp/qshell/qshell account $QINIU_ACCESS_KEY $QINIU_SECRET_KEY
-/bin/bash /tmp/qshell/qshell qupload /jekyll/qiniu/qupload_assets.json
-/bin/bash /tmp/qshell/qshell qupload /jekyll/qiniu/qupload_files.json
+/bin/bash /tmp/qshell/qshell_linux_amd64 account $QINIU_ACCESS_KEY $QINIU_SECRET_KEY
+/bin/bash /tmp/qshell/qshell_linux_amd64 qupload /jekyll/qiniu/qupload_assets.json
+/bin/bash /tmp/qshell/qshell_linux_amd64 qupload /jekyll/qiniu/qupload_files.json
 
 # jekyll build
 jekyll serve
