49990fbaf9fa217d5ea4040e3de7fbb5616ca167
dan <i@shanhh.com>
2016-12-25 12:56:23 +0800

    feature update cdn domain

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+service nginx start
 
 # /jekyll/tools/qshell/qshell_linux_amd64 account $QINIU_ACCESS_KEY $QINIU_SECRET_KEY
 
@@ -7,8 +8,6 @@ sed -i 's/QINIU_ACCESS_KEY/'"$QINIU_ACCESS_KEY"'/g' /jekyll/qiniu/qupload_*.json
 sed -i 's/QINIU_SECRET_KEY/'"$QINIU_SECRET_KEY"'/g' /jekyll/qiniu/qupload_*.json
 /jekyll/tools/qshell/qshell_linux_amd64 qupload /jekyll/qiniu/qupload_assets.json
 /jekyll/tools/qshell/qshell_linux_amd64 qupload /jekyll/qiniu/qupload_files.json
-
-service nginx start
 
 while :; do
   sleep 3000
