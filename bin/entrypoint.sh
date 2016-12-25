2e21e2e50e98745bc407469443a5fcb9ef976901
dan <i@shanhh.com>
2016-12-25 12:44:30 +0800

    feature update docker file

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+
+# /jekyll/tools/qshell/qshell_linux_amd64 account $QINIU_ACCESS_KEY $QINIU_SECRET_KEY
+
+sed -i 's/QINIU_ACCESS_KEY/'"$QINIU_ACCESS_KEY"'/g' /jekyll/qiniu/qupload_*.json
+sed -i 's/QINIU_SECRET_KEY/'"$QINIU_SECRET_KEY"'/g' /jekyll/qiniu/qupload_*.json
+/jekyll/tools/qshell/qshell_linux_amd64 qupload /jekyll/qiniu/qupload_assets.json
+/jekyll/tools/qshell/qshell_linux_amd64 qupload /jekyll/qiniu/qupload_files.json
+
 service nginx start
-
-# /tmp/qshell/qshell_linux_amd64 account $QINIU_ACCESS_KEY $QINIU_SECRET_KEY
-
-#sed -i 's/QINIU_ACCESS_KEY/'"$QINIU_ACCESS_KEY"'/g' /jekyll/qiniu/qupload_*.json
-#sed -i 's/QINIU_SECRET_KEY/'"$QINIU_SECRET_KEY"'/g' /jekyll/qiniu/qupload_*.json
-#/tmp/qshell/qshell_linux_amd64 qupload /jekyll/qiniu/qupload_assets.json
-#/tmp/qshell/qshell_linux_amd64 qupload /jekyll/qiniu/qupload_files.json
 
 while :; do
   sleep 3000
